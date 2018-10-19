70912db1aefabffa53840fc8fd80a14ddccac16b
Muosvr <lujason2015@gmail.com>
2018-10-19 00:34:04 -0400

    task list and availability calendar

--- scripts/script.js
+++ scripts/script.js
@@ -19,6 +19,24 @@ function run(){
   //   console.log(e.target);
   // })
 
+  var listItem = "<li draggable='true' class='sortable-bulk listItem'>"
+                + "<input placeholder='task' class='task' type='text'></input>"
+                +"<input placeholder='deadline' class='datepicker' type='date'></input>"
+                + "<input placeholder='hours' class='duration' type='number'></input>"
+                + "<button class='deleteBtn' action=delete()>Delete</button>"
+                + "</li>"
+
+  $("#addButton").click(function(){
+    $("ul").append(listItem);
+    $("ul li:last .deleteBtn").click(function(){
+      list = $(this)[0].parentNode;
+      console.log(list);
+      list.remove();
+    })
+  })
+
+
+
   document.addEventListener('dragstart', function(event) {
   		dragging = event.target;
       event.dataTransfer.setData('text/html', dragging);
@@ -55,4 +73,35 @@ function run(){
       }
   });
 
+}
+
+
+function schedule_task(){
+  var list = $(".listItem");
+  var length = list.length;
+  var availability = {
+    "Mon": 0,
+    "Tue": 0,
+    "Wed": 0,
+    "Thur": 0,
+    "Fri": 0,
+    "Sat": 0,
+    "Sun": 0
+  }
+
+  for(i=0; i<7; i++){
+    var id = $("td")[i].getElementsByTagName("Input")[0].id;
+    var value = $("td")[i].getElementsByTagName("Input")[0].value;
+    if (value != ""){
+      availability[id] = parseInt(value);
+    }
+
+  }
+
+  console.log(availability)
+
+  for(i=0; i<length; i++){
+
+  }
+  console.log(list.length)
 }
